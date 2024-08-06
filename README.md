1c3bfe91847fdd65aa8b74ef4b739cbfbe994ed2
Jeff Martin <jeffreymartin@outlook.com>
2024-08-05 20:07:00 -0500

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # splitter
-An audio file splitter
+Automatically splits audio files in a directory to make smaller files. This is useful if you don't want to have to load an entire audio file in memory. You can optionally delete the original files.
+
+## Optional command-line parameters:
+`-f`, `--folder`: The directory to convert. Defaults to `"."`
+
+`-n`, `--num_frames`: The maximum number of frames per file. Defaults to `44100 * 10`
+
+`-t`, `--num-threads`: The maximum number of threads to run. If `0` (default), the program uses the same number of threads as there are processors available.
+
+## Optional command-line flags
+`-d`, `--delete`: If you provide this flag, the original files will be deleted.
+
+`-i`, `--fade-in`: If you provide this flag, a very brief fade in / fade out will be added to the new audio files.
+
+## License
+This program is provided under the GNU GPL 3.0 license.
---
+++ src/main.rs
@@ -0,0 +1,214 @@
+use aus::AudioError;
+use glob::glob;
+use threadpool::ThreadPool;
+
+/// Finds all audio files that match a search pattern.
+/// For each file, returns a tuple that has the full path, directory, and file name without extension.
+fn find_audio(directory: &str) -> Vec<(String, String, String)> {
+    let mut file_paths: Vec<(String, String, String)> = Vec::new();
+    let extensions = vec!["aif", "aiff", "mp3", "flac", "ogg", "aac", "m4a", "wma"];
+    for extension in extensions {
+        let entries = glob(&format!("{}/**/*.{}", directory, extension));
+        match entries {
+            Ok(paths) => {
+                for entry in paths {
+                    match entry {
+                        Ok(path) => {
+                            let file_name = match path.file_stem() {
+                                Some(x) => match x.to_str() {
+                                    Some(y) => String::from(y),
+                                    None => String::from("")
+                                },
+                                None => String::from("")
+                            };
+                            let mut parent = path.clone();
+                            parent.pop();
+                            let parent = match parent.to_str() {
+                                Some(x) => String::from(x),
+                                None => String::from("")
+                            };
+                            let path = match path.to_str() {
+                                Some(x) => String::from(x),
+                                None => String::from("")
+                            };
+                            file_paths.push((path, parent, file_name));
+                        },
+                        Err(_) => ()
+                    };
+                }
+            },
+            Err(_) => ()
+        }
+    }
+    file_paths
+}
+
+/// Processes all of the files in a file vector and converts them to WAV
+fn process(files: &Vec<(String, String, String)>, max_num_frames: usize, max_num_threads: usize, fade_in: bool) {
+    let max_available_threads = match std::thread::available_parallelism() {
+        Ok(x) => x.get(),
+        Err(_) => 1
+    };
+
+    let num_threads = if max_num_threads < 1 {
+        max_available_threads
+    } else {
+        usize::min(max_available_threads, max_num_threads)
+    };
+
+    let pool = ThreadPool::new(num_threads);
+    for file_tup in files.iter() {
+        let file = file_tup.0.clone();
+        let dir = file_tup.1.clone();
+        let name = file_tup.2.clone();
+        pool.execute(move || {
+            println!("File: {}", file);
+            match aus::read(&file) {
+                Ok(mut audio) => {
+                    // Calculate the number of files to split into
+                    let mut num_files = audio.num_frames / max_num_frames;
+                    if num_files * max_num_frames < audio.num_frames {
+                        num_files += 1;
+                    }
+                    
+                    // Fill missing information if necessary
+                    if audio.bits_per_sample == 0 {
+                        audio.bits_per_sample = 16;
+                        audio.audio_format = aus::AudioFormat::S16;
+                    }
+                    if audio.sample_rate == 0 {
+                        audio.sample_rate = 44100;
+                    }
+
+                    // Split the file
+                    for i in 0..num_files {
+                        let new_file_name = format!("{}/{}_chunk{:0>3}.wav", dir, name, i);
+                        let start_idx = i * max_num_frames;
+                        let end_idx = usize::min(start_idx + max_num_frames, audio.num_frames);
+                        let mut new_samples: Vec<Vec<f64>> = Vec::new();
+                        for j in 0..audio.num_channels {
+                            new_samples.push(Vec::new());
+                            for k in start_idx..end_idx {
+                                new_samples[j].push(audio.samples[j][k]);
+                            }
+                        }
+                        if fade_in {
+                            for j in 0..audio.num_channels {
+                                aus::operations::fade_in(&mut new_samples[j], aus::WindowType::Hanning, 1000);
+                                aus::operations::fade_out(&mut new_samples[j], aus::WindowType::Hanning, 1000);
+                            }
+                        }
+                        let new_audio = aus::AudioFile::new(audio.audio_format, audio.sample_rate, new_samples);
+                        match aus::write(&new_file_name, &new_audio) {
+                            Ok(_) => (),
+                            Err(err) => match err {
+                                AudioError::FileCorrupt => println!("Error writing file {}: The file was corrupt.", new_file_name),
+                                AudioError::FileInaccessible(msg) => println!("Error writing file {}: The file was inaccessible ({}).", new_file_name, msg),
+                                AudioError::NumChannels(msg) => println!("Error writing file {}: The number of channels was wrong ({}).", new_file_name, msg),
+                                AudioError::NumFrames(msg) => println!("Error writing file {}: The number of frames was wrong ({}).", new_file_name, msg),
+                                AudioError::SampleValueOutOfRange(msg) => println!("Error writing file {}: A sampel value was out of range ({}).", new_file_name, msg),
+                                AudioError::WrongFormat(msg) => println!("Error writing file {}: The format was wrong ({}).", new_file_name, msg)
+                            }
+                        };    
+                    }
+                },
+                Err(err) => match err {
+                    AudioError::FileCorrupt => println!("Error writing file {}: The file was corrupt.", file),
+                    AudioError::FileInaccessible(msg) => println!("Error writing file {}: The file was inaccessible ({}).", file, msg),
+                    AudioError::NumChannels(msg) => println!("Error writing file {}: The number of channels was wrong ({}).", file, msg),
+                    AudioError::NumFrames(msg) => println!("Error writing file {}: The number of frames was wrong ({}).", file, msg),
+                    AudioError::SampleValueOutOfRange(msg) => println!("Error writing file {}: A sampel value was out of range ({}).", file, msg),
+                    AudioError::WrongFormat(msg) => println!("Error writing file {}: The format was wrong ({}).", file, msg)
+                }
+            }
+        });
+    }
+
+    pool.join();
+}
+
+struct Args {
+    folder: String,
+    num_frames: usize,
+    num_threads: usize,
+    fade_in: bool,
+    delete: bool
+}
+
+/// Validates the command line arguments
+fn validate_args(args: Vec<String>) -> Option<Args> {
+    if args.len() <= 8 {
+        let valid_args = std::collections::HashMap::from([("-f", 1), ("--folder", 1), ("-n", 1), ("--num-threads", 1), ("-d", 1), ("--delete", 1)]);
+        let mut processed_args: Args = Args{folder: String::from("."), num_frames: 44100 * 10, num_threads: 0, fade_in: false, delete: false};
+        let mut i = 1;
+        while i < args.len() {
+            if !valid_args.contains_key(args[i].as_str()) {
+                return None;
+            } else {
+                match args[i].as_str() {
+                    "-f" | "--folder" => {
+                        processed_args.folder = args[i+1].clone();
+                        i += 2;
+                    },
+                    "-n" | "--num-frames" => {
+                        processed_args.num_frames = match args[i+1].parse::<usize>() {
+                            Ok(x) => x,
+                            Err(_) => return None
+                        };
+                        i += 2;
+                    },
+                    "-t" | "--num-threads" => {
+                        processed_args.num_threads = match args[i+1].parse::<usize>() {
+                            Ok(x) => x,
+                            Err(_) => return None
+                        };
+                        i += 2;
+                    },
+                    "-d" | "--delete" => {
+                        processed_args.fade_in = true;
+                        i += 1;
+                    },
+                    "-i" | "--fade-in" => {
+                        processed_args.delete = true;
+                        i += 1;
+                    },
+                    _ => {
+                        return None;
+                    }
+                }
+            }
+        }
+
+        return Some(processed_args);
+    } else {
+        return None;
+    }
+}
+
+fn main() {
+    // process the arguments
+    let args = match validate_args(std::env::args().collect()) {
+        Some(x) => x,
+        None => {
+            println!("Usage:\n-f folder_name -n num_frames -t num_threads\nOptional: include the -d flag to delete the original files when done, and/or the -i flag to add a brief fade in/fade out to the new files.");
+            return;
+        }
+    };
+    
+    // convert the files
+    let files = find_audio(&args.folder);
+    println!("Splitting {} files...", files.len());
+    process(&files, args.num_frames, args.num_threads, args.fade_in);
+
+    // delete the old files if asked to
+    if args.delete {
+        println!("Deleting original files...");
+        for file in files.iter() {
+            match std::fs::remove_file(file.0.as_str()) {
+                Ok(_) => (),
+                Err(_) => ()
+            };
+        }
+    }
+    println!("Done.");
+}
